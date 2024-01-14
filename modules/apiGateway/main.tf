d3f6815d4ea4c3437b2f715d5a2c82c2d38741c4
devakshaygupta <akshaygupta1997@rediffmail.com>
2024-01-14 22:39:45 +0530

    fixed issues in apigconfiguration

--- modules/apiGateway/main.tf
+++ modules/apiGateway/main.tf
@@ -19,13 +19,36 @@ resource "aws_api_gateway_method" "get-user-method" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.ServerlessRESTAPI.id
+  resource_id = aws_api_gateway_resource.get-users.id
+  http_method = aws_api_gateway_method.get-user-method.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Content-Type" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "lambdaIntegration" {
   rest_api_id             = aws_api_gateway_rest_api.ServerlessRESTAPI.id
   resource_id             = aws_api_gateway_resource.get-users.id
   http_method             = aws_api_gateway_method.get-user-method.http_method
   integration_http_method = "POST"
   type                    = "AWS"
   uri                     = module.lambda.get_user_function_arn
+}
+
+resource "aws_api_gateway_integration_response" "lambdaIntegrationResponse" {
+  rest_api_id = aws_api_gateway_rest_api.ServerlessRESTAPI.id
+  resource_id = aws_api_gateway_resource.get-users.id
+  http_method = aws_api_gateway_method.get-user-method.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+  response_parameters = {
+    "method.response.header.Content-Type" = "'application/json'"
+  }
+  depends_on = [
+    aws_api_gateway_integration.lambdaIntegration
+  ]
 }
 
 # Lambda
@@ -35,15 +58,29 @@ resource "aws_lambda_permission" "apigw_lambda" {
   function_name = var.get-users-function-name
   principal     = "apigateway.amazonaws.com"
 
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.ServerlessRESTAPI.id}/*/${aws_api_gateway_method.get-user-method.http_method}${aws_api_gateway_resource.get-users.path}"
+}
+
+resource "aws_api_gateway_model" "userModel" {
+  rest_api_id  = aws_api_gateway_rest_api.ServerlessRESTAPI.id
+  name         = "userModel"
+  description  = "a user object schema"
+  content_type = "application/json"
+
+  schema = jsonencode({
+    type = "object"
+  })
 }
 
 resource "aws_api_gateway_deployment" "get-users-deployment" {
   rest_api_id = aws_api_gateway_rest_api.ServerlessRESTAPI.id
 
   triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.ServerlessRESTAPI.body))
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.get-users.id,
+      aws_api_gateway_method.get-user-method.id,
+      aws_api_gateway_integration.lambdaIntegration.id
+    ]))
   }
 
   lifecycle {
@@ -55,5 +92,5 @@ resource "aws_api_gateway_stage" "get-users-v1-stage" {
   deployment_id = aws_api_gateway_deployment.get-users-deployment.id
   rest_api_id   = aws_api_gateway_rest_api.ServerlessRESTAPI.id
   stage_name    = "v1"
-  description = "Initial rollout of the API!"
+  description   = "Initial rollout of the API!"
 }
